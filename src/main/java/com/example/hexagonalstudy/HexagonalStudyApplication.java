5b62b854dae6dcbb98a5822acce03d29512218ef
gorapaduckoo <jioung93@naver.com>
2024-03-09 02:34:01 +0900

    Initial commit

---
+++ src/main/java/com/example/hexagonalstudy/HexagonalStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hexagonalstudy/HexagonalStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
