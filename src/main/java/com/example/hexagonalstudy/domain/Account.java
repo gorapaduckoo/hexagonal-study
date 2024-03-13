206e3a9398c5df2e32201beecd7db94151eb8110
gorapaduckoo <jioung93@naver.com>
2024-03-14 01:10:51 +0900

    도메인 모델 구현하기

---
+++ src/main/java/com/example/hexagonalstudy/domain/Account.java
@@ -0,0 +1,58 @@
+package com.example.hexagonalstudy.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Account {
+
+    private final AccountId id;
+    private final Money baselineBalance;
+    private final ActivityWindow activityWindow;
+
+    public static Account withoutId(Money baselineBalance, ActivityWindow activityWindow) {
+        return new Account(null, baselineBalance, activityWindow);
+    }
+
+    public static Account withId(AccountId accountId, Money baselineBalance, ActivityWindow activityWindow) {
+        return new Account(accountId, baselineBalance, activityWindow);
+    }
+
+    public Optional<AccountId> getId() {
+        return Optional.ofNullable(this.id);
+    }
+
+    public Money calculateBalance() {
+        return Money.add(
+                this.baselineBalance,
+                this.activityWindow.calculateBalance(this.id));
+    }
+
+    public boolean withdraw(Money money, AccountId targetAccountId) {
+
+        if(!mayWithdraw(money)) {
+            return false;
+        }
+
+        Activity withdrawl = new Activity(
+                this.id,
+                this.id,
+                targetAccountId,
+                LocalDateTime.now(),
+                money);
+        this.activityWindow.addActivity(withdrawl);
+        return true;
+    }
+
+    private boolean mayWithdraw(Money money) {
+        return Money.add(this.calculateBalance(), money.negate())
+                .isPositiveOrZero();
+    }
+    @Value
+    public static class AccountId {
+        Long value;
+    }
+}
---
+++ src/main/java/com/example/hexagonalstudy/domain/ActivityWindow.java
@@ -0,0 +1,48 @@
+package com.example.hexagonalstudy.domain;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Value
+@RequiredArgsConstructor
+public class ActivityWindow {
+
+    @NonNull List<Activity> activities;
+
+    public LocalDateTime getStartTimestamp() {
+        return activities.stream()
+                .min(Comparator.comparing(Activity::getTimestamp))
+                .orElseThrow(IllegalStateException::new)
+                .getTimestamp();
+    }
+
+    public Money calculateBalance(Account.AccountId accountId) {
+        Money depositBalance = activities.stream()
+                .filter(a -> a.getTargetAccountId().equals(accountId))
+                .map(Activity::getMoney)
+                .reduce(Money.ZERO, Money::add);
+
+        Money withdrawalBalance = activities.stream()
+                .filter(a -> a.getSourceAccountId().equals(accountId))
+                .map(Activity::getMoney)
+                .reduce(Money.ZERO, Money::add);
+
+        return Money.add(depositBalance, withdrawalBalance.negate());
+    }
+
+    public ActivityWindow(@NonNull Activity... activities) {
+        this.activities = new ArrayList<>(Arrays.asList(activities));
+    }
+
+    public List<Activity> getActivities() {
+        return Collections.unmodifiableList(this.activities);
+    }
+
+    public void addActivity(Activity activity) {
+        this.activities.add(activity);
+    }
+}
