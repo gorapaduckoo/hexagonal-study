fcc27a6b44a153812abf37065727310eb955fbb1
gorapaduckoo <jioung93@naver.com>
2024-03-26 22:42:52 +0900

    입력 유효성 검증

---
+++ src/main/java/com/example/common/SelfValidationg.java
@@ -0,0 +1,22 @@
+package com.example.common;
+
+import jakarta.validation.*;
+
+import java.util.Set;
+
+public abstract class SelfValidationg<T> {
+
+    private Validator validator;
+
+    public SelfValidationg() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    protected void validateSelf() {
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
--- src/main/java/com/example/hexagonalstudy/application/port/in/SendMoneyCommand.java
+++ src/main/java/com/example/hexagonalstudy/application/port/in/SendMoneyCommand.java
@@ -1,4 +1,32 @@
 package com.example.hexagonalstudy.application.port.in;
 
-public class SendMoneyCommand {
+import com.example.common.SelfValidationg;
+import com.example.hexagonalstudy.domain.Account;
+import com.example.hexagonalstudy.domain.Money;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class SendMoneyCommand extends SelfValidationg<SendMoneyCommand> {
+
+    @NotNull
+    private final Account.AccountId sourceAccountId;
+    @NotNull
+    private final Account.AccountId targetAccountId;
+    @NotNull
+    private final Money money;
+
+    public SendMoneyCommand(Account.AccountId sourceAccountId, Account.AccountId targetAccountId, Money money) {
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.money = money;
+        requireGreaterThan(money, 0);
+        this.validateSelf();
+    }
+
+    private void requireGreaterThan(Money money, int amount) {
+        // Validate money is greater than amount
+    }
 }
